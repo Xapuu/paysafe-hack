00bbd64e350c9ea2f10110a8db5ece1d33300250
VOLCOM071191 <maikl.nelis@googlemail.com>
2019-06-02 19:39:59 +0200

    Create User.js

---
+++ controlers/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const encrypt = require('./../utils/crypto')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.Schema.Types.String,
+    unique: true
+  },
+  balance: {
+    type: mongoose.Schema.Types.Number,
+    default: 0
+  },
+  salt: {
+    type: mongoose.Schema.Types.String,
+    default: encrypt.generateSalt(),
+    required: true
+  },
+  hashedPass: { type: mongoose.Schema.Types.String, required: true }
+})
+
+userSchema.methods.authenticate = password =>
+  this.hashedPass === encrypt.generateHashedPassword(this.salt, password)
+
+userSchema.pre('save', function () {
+  this.salt = encrypt.generateSalt()
+  this.hashedPass = encrypt.generateHashedPassword(this.salt, this.hashedPass)
+})
+
+module.exports = mongoose.model('User', userSchema)
